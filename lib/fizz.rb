d2f0cb003a745bd91e98b9863f2b083ca79298fa
Jack Henley <jack.d.henley@gmail.com>
2014-01-15 09:29:35 -0500

    completed, bit late

--- lib/fizz.rb
+++ lib/fizz.rb
@@ -1,7 +1,10 @@
 
-def fizz_buzz(num)
-	case num
-	  when (num % 3) == 0
-	  	return 'fizz'
-  end
+def fizz_buzz(x)
+	if x % 3 == 0 && x % 5 == 0
+		'fizz buzz'
+	elsif x % 5 == 0
+		'buzz'
+	elsif x % 3 == 0
+		'fizz'
+	end
 end
--- spec/fizz_spec.rb
+++ spec/fizz_spec.rb
@@ -3,14 +3,24 @@ require_relative '../lib/fizz'
 
 describe '#fizz_buzz' do
 
-	it 'should take a single argument of x' do
-	x = x
-	expect(fizz_buzz(x)).to eq x
-	end
+	# it 'should take a single argument of x' do
+	# x = x
+	# expect(fizz_buzz(x)).to eq x
+	# end
 
 	it 'should return fizz if x is divisible by 3' do
 		number = 9
 		expect(fizz_buzz(number)).to eq 'fizz'
+	end
+
+	it 'should return buzz if x is divisible by 5' do
+		number = 10
+		expect(fizz_buzz(number)).to eq 'buzz'
+	end
+
+	it 'should return fizz if x is divisible by 3' do
+		number = 15
+		expect(fizz_buzz(number)).to eq 'fizz buzz'
 	end
 
 
