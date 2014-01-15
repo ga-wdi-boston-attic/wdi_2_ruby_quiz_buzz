9e0ec58c1b8fbe1f1cc4b174c61ac11c7f58a856
Elisabeth Waltz <elisabeth.waltz@gmail.com>
2014-01-15 09:21:15 -0500

    first commit - not totally done, just don't want a zero

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,16 @@
+require relative "spec_helper"
+
+describe fizzbuzz do |variable|
+  it 'will take a single input argument of x'
+   expect
+  end
+  it 'will return a string of "fizz" for numbers divisible by 3'
+    expect
+  end
+  it 'will return a string of "buzz" for numbers divisible by 5'
+    expect
+  end
+  it 'will return a string of "fizzbuzz" for numbers divisible by both 3 and 5'
+    expect
+  end
+end
