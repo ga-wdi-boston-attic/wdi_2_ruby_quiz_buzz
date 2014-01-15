3383b0936c5efd2dafa2121631d3acd9f37e2f0b
Mike Stone <mikestone14@gmail.com>
2014-01-15 09:27:07 -0500

    Refactored fizzbuzz

--- lib/main.rb
+++ lib/main.rb
@@ -1,11 +1,6 @@
 def fizzbuzz(x)
-	if (x % 5 == 0) && (x % 3 == 0)
-		"fizzbuzz"
-	elsif (x % 3 == 0)
-		"fizz"
-	elsif (x % 5 == 0)
-		"buzz"
-	else
-		"The number #{x} is not divisible by 3 or 5"
-	end
+	string = ''
+	string += 'fizz' if x % 3 == 0
+	string += 'buzz' if x % 5 == 0
+	string
 end
